ac5b305ee54d1ac1a66a09ce182722f1e9744bc6
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-02-29 17:22:10 -0700

    changin routes?

--- routes/items.js
+++ routes/items.js
@@ -4,9 +4,9 @@ const { auth, requiresAuth } = require('express-openid-connect');
 
 router.get('/', itemsController.getAll);
 router.get('/:id', itemsController.getSingle);
-router.post('/', requiresAuth(), itemsController.createItem);
-router.put('/:id', requiresAuth(), itemsController.updateItem);
-router.delete('/:id', requiresAuth(), itemsController.deleteItem);
+router.post('/post', requiresAuth(), itemsController.createItem);
+router.put('/put/:id', requiresAuth(), itemsController.updateItem);
+router.delete('/delete/:id', requiresAuth(), itemsController.deleteItem);
 
 router.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
