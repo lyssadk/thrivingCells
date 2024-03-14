3bca988cffd0a8320b225bffdc92bed6f7e2675b
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-03-13 19:17:30 -0600

    I swear lol

--- controllers/items.js
+++ controllers/items.js
@@ -12,7 +12,7 @@ const getAll = async (req, res, next) => {
 
 const getSingle = async (req, res, next) => {
     const userId = req.params.id;
-    const result = await mongodb.getDb().db().collection('items').find({ _id: userId });
+    const result = await mongodb.getDb().db().collection('Items').find({ _id: userId });
     result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists[0]);
@@ -20,26 +20,19 @@ const getSingle = async (req, res, next) => {
   };
 
 
-  const createItem = async (req, res) => {
-    try {
-      const item = {
-        name: req.body.name,
-        quantity: req.body.quantity,
-        wholesale: req.body.wholesale,
-      };
-  
-      const response = await mongodb.getDb().db().collection('items').insertOne(item);
-  
-      if (response.acknowledged) {
-        res.status(201).json(response.ops[0]); // Send the inserted item in the response
-      } else {
-        res.status(500).json({ error: 'Some error occurred while creating the item.' });
-      }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
+const createItem = async (req, res) => {
+  const item = {
+    name: req.body.name,
+    quantity: req.body.quantity,
+    wholesale: req.body.wholesale,
   };
-  
+  const response = await mongodb.getDb().db().collection('Items').insertOne(item);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  }
+};
 
 const updateItem = async (req, res) => {
   const userId = new ObjectId(req.params.id);
